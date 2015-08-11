415c4e3764a1136f4f4f92a5ab8d5372b04f43f1
Manish Sharma <manishsharma@Manishs-MacBook-Pro.local>
2015-08-10 21:20:59 -0400

    Initial Commit

---
+++ Roshambo_M/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Roshambo_M
+//
+//  Created by Manish Sharma on 8/10/15.
+//  Copyright (c) 2015 CelG Mobile LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
